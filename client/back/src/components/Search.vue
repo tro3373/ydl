ea989504199cc0f8cd82ad9007f1c88c1931bdb4
tro3373 <tro3373@gmail.com>
2021-01-02 17:55:54 +0900

    feat: add search vue

---
+++ client/back/src/components/Search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello"></div>
+</template>
+
+<script>
+export default {
+  name: 'Search'
+  // props: {
+  //   msg: String
+  // }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
