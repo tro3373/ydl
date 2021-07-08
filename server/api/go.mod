e006f60176ec20cd0ec6806b1e58e8cedfa1e637
tro3373 <tro3373@gmail.com>
2021-07-09 08:28:07 +0900

    feat: Update api

--- server/api/go.mod
+++ server/api/go.mod
@@ -1,6 +1,6 @@
 module github.com/tro3373/ydl
 
-go 1.15
+go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.6.3
@@ -11,6 +11,8 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/ugorji/go v1.2.1 // indirect
+	go.mongodb.org/mongo-driver v1.4.5
+	go.uber.org/zap v1.17.0 // indirect
 	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
 	golang.org/x/sys v0.0.0-20201214210602-f9fddec55a1e // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
--- server/api/main.go
+++ server/api/main.go
@@ -3,12 +3,171 @@ package main
 import (
 	"net/http"
 
+	"context"
+	"log"
+	"time"
+
 	"github.com/gin-gonic/gin"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/tro3373/ydl/middleware"
 )
+
+func insertBsonD(col *mongo.Collection) error {
+	bsonD := bson.D{
+		{Key: "str1", Value: "abc"},
+		{Key: "num1", Value: 1},
+		{Key: "str2", Value: "xyz"},
+		{Key: "num2", Value: bson.A{2, 3, 4}},
+		{Key: "subdoc", Value: bson.D{{Key: "str", Value: "subdoc"}, {Key: "num", Value: 987}}},
+		{Key: "date", Value: time.Now()},
+	}
+	_, err := col.InsertOne(context.Background(), bsonD)
+	return err
+}
+
+func insertBsonM(col *mongo.Collection) error {
+	bsonM := bson.M{
+		"str1":   "efg",
+		"num1":   11,
+		"str2":   "opq",
+		"num2":   bson.A{12, 13, 14},
+		"subdoc": bson.M{"str": "subdoc", "num": 987},
+		"date":   time.Now(),
+	}
+	for i := 0; i < 10; i++ {
+		_, err := col.InsertOne(context.Background(), bsonM)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type myType struct {
+	Str1   string
+	Num1   int
+	Str2   string
+	Num2   []int
+	Subdoc struct {
+		Str string
+		Num int
+	}
+	Date time.Time
+}
+
+func insertStruct(col *mongo.Collection) error {
+	doc := myType{
+		"hij",
+		21,
+		"rst",
+		[]int{22, 23, 24},
+		struct {
+			Str string
+			Num int
+		}{"subdoc", 987},
+		time.Now(),
+	}
+	_, err := col.InsertOne(context.Background(), doc)
+	return err
+}
+
+func insertNextBsonM(col *mongo.Collection) error {
+	var num int32
+	var err error
+	if num, err = findMaxNum(col); err != nil {
+		return err
+	}
+	// log.Printf(">>> findedNum: %d\n", num)
+	num++
+	// log.Printf(">>> addedNum: %d\n", num)
+	bsonM := bson.M{
+		"str1":   "efg",
+		"num1":   num,
+		"str2":   "opq",
+		"num2":   bson.A{12, 13, 14},
+		"subdoc": bson.M{"str": "subdoc", "num": 987},
+		"date":   time.Now(),
+	}
+	log.Printf("> Inserting: %d\n", num)
+	_, err = col.InsertOne(context.Background(), bsonM)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func findMaxNum(col *mongo.Collection) (int32, error) {
+
+	var ret int32
+	options := options.Find()
+	options.SetSort(bson.D{bson.E{Key: "num1", Value: -1}})
+	options.SetLimit(1)
+	cursor, err := col.Find(context.Background(), bson.D{}, options)
+	if err != nil {
+		return ret, err
+	}
+	for cursor.Next(context.Background()) {
+		elem := bson.M{}
+		err := cursor.Decode(&elem)
+		if err != nil {
+			return ret, err
+		}
+		ret = elem["num1"].(int32)
+		// log.Printf(">> Max: %d\n", ret)
+		// results = append(results, *elem)
+		// lastValue = elem.Lookup("_id")
+	}
+	// log.Printf(">> Returning: %d\n", ret)
+	return ret, nil
+}
+
+func mainMain() error {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017/?connect=direct"))
+	if err != nil {
+		return err
+	}
+	if err = client.Connect(context.Background()); err != nil {
+		return err
+	}
+	defer client.Disconnect(context.Background())
+
+	col := client.Database("test").Collection("col")
+	// if err = insertBsonD(col); err != nil {
+	//     return err
+	// }
+	// if err = insertBsonM(col); err != nil {
+	//     return err
+	// }
+	// if err = insertStruct(col); err != nil {
+	//     return err
+	// }
+	for i := 0; i < 10000; i++ {
+		// if _, err = findMaxNum(col); err != nil {
+		//     return err
+		// }
+		if err = insertNextBsonM(col); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// func main() {
+//     if err := mainMain(); err != nil {
+//         log.Fatal(err)
+//     }
+//     log.Println("normal end.")
+// }
 
 func main() {
 	engine := gin.Default()
-	engine.GET("/", func(c *gin.Context) {
+	engine.Use(middleware.RecordUaAndTime)
+
+	engine.GET("/api", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "hello world",
 		})
