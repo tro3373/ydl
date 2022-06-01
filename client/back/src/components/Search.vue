c7f90a8be85f399892f221552a21ad0705bbe603
tro3373 <tro3373@gmail.com>
2022-06-01 21:28:44 +0900

    feat: Add 'No Result' container if empty

--- client/back/src/components/Search.vue
+++ client/back/src/components/Search.vue
@@ -82,23 +82,17 @@
       </v-flex>
 
       <v-flex xs12 sm12 md8 class="pa-1">
-        <v-card>
+        <v-container fill-height v-if="!doneList.length">
+          <v-spacer></v-spacer>
+          No Result
+          <v-spacer></v-spacer>
+        </v-container>
+        <v-card v-if="doneList.length">
           <v-toolbar color="primary" dark>
-            <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
             <v-toolbar-title>Files</v-toolbar-title>
-            <!-- <v-spacer></v-spacer> -->
-            <!-- <v-btn icon>                   -->
-            <!--   <v-icon>mdi-magnify</v-icon> -->
-            <!-- </v-btn>                       -->
-
-            <!-- <v-btn icon>                       -->
-            <!--   <v-icon>mdi-view-module</v-icon> -->
-            <!-- </v-btn>                           -->
           </v-toolbar>
 
-          <!-- <v-list subheader two-line> -->
           <v-list two-line>
-            <!-- <v-subheader inset>Folders</v-subheader> -->
             <v-list-item
               v-for="done in doneList"
               :key="done.url"
@@ -126,22 +120,6 @@
                 </div>
               </v-list-item-action>
             </v-list-item>
-            <!-- <v-divider inset></v-divider>                                            -->
-            <!-- <v-subheader inset>Files</v-subheader>                                   -->
-            <!-- <v-list-item v-for="file in files" :key="file.title">                    -->
-            <!--   <v-list-item-avatar>                                                   -->
-            <!--     <v-icon :class="file.color" dark v-text="file.icon"></v-icon>        -->
-            <!--   </v-list-item-avatar>                                                  -->
-            <!--   <v-list-item-content>                                                  -->
-            <!--     <v-list-item-title v-text="file.title"></v-list-item-title>          -->
-            <!--     <v-list-item-subtitle v-text="file.subtitle"></v-list-item-subtitle> -->
-            <!--   </v-list-item-content>                                                 -->
-            <!--   <v-list-item-action>                                                   -->
-            <!--     <v-btn icon>                                                         -->
-            <!--       <v-icon color="grey lighten-1">mdi-information</v-icon>            -->
-            <!--     </v-btn>                                                             -->
-            <!--   </v-list-item-action>                                                  -->
-            <!-- </v-list-item>                                                           -->
           </v-list>
         </v-card>
       </v-flex>
